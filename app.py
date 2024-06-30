b19201703adc9c2024ddcf19cc51611792fbdeae
KeerthanaDevi2004 <159637088+KeerthanaDevi2004@users.noreply.github.com>
2024-06-30 21:33:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pickle
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import SelectKBest, chi2
+import category_encoders as ce
+
+class CustomLabelEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        self.encoders = {col: LabelEncoder() for col in columns}
+
+    def fit(self, X, y=None):
+        for col in self.columns:
+            self.encoders[col].fit(X[col])
+        return self
+
+    def transform(self, X):
+        X_copy = X.copy()
+        for col in self.columns:
+            X_copy[col] = self.encoders[col].transform(X[col])
+        return X_copy
+
+    def inverse_transform(self, X):
+        X_copy = X.copy()
+        for col in self.columns:
+            X_copy[col] = self.encoders[col].inverse_transform(X[col])
+        return X_copy
+
+app = Flask(__name__)
+
+print("Loading pipeline...")
+pipeline = joblib.load('pipeline_1.pkl')
+print("Pipeline loaded successfully.")
+
+print("Loading unique values...")
+with open('unique_values.pkl', 'rb') as file:
+    unique_values = pickle.load(file)
+print("Unique values loaded successfully.")
+
+@app.route('/')
+def index():
+    print("Rendering index.html...")
+    return render_template('index.html', unique_values=unique_values)
+@app.route('/about-us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        print("Request received. Parsing JSON data...")
+        data = request.get_json(force=True)
+        input_data = pd.DataFrame([data])  # Create DataFrame from JSON data
+        print("Input data:")
+        print(input_data)
+        
+        new_column_names = {
+            'days_shipping_real': 'Days for shipping (real)',
+            'days_shipment_scheduled': 'Days for shipment (scheduled)'
+        }
+        input_data.rename(columns=new_column_names, inplace=True)
+        print("Renamed input data:")
+        print(input_data)
+
+        ans = pipeline.predict(input_data)
+        print("Prediction:", ans)
+        return jsonify({'prediction': int(ans[0])})
+
+    except Exception as e:
+        print(f"Prediction error: {str(e)}")
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import pickle
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_selection import SelectKBest, chi2
+import category_encoders as ce
+import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class CustomLabelEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        self.encoders = {col: LabelEncoder() for col in columns}
+
+    def fit(self, X, y=None):
+        for col in self.columns:
+            self.encoders[col].fit(X[col])
+        return self
+
+    def transform(self, X):
+        X_copy = X.copy()
+        for col in self.columns:
+            X_copy[col] = self.encoders[col].transform(X[col])
+        return X_copy
+
+    def inverse_transform(self, X):
+        X_copy = X.copy()
+        for col in self.columns:
+            X_copy[col] = self.encoders[col].inverse_transform(X[col])
+        return X_copy
+
+def main():
+    data = pd.read_csv("C:Users/ALMIGHTY/Downloads/late/cleaned4_late.csv")
+    y = data['Late_delivery_risk']
+    X = data.drop(columns=['Late_delivery_risk'])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Define the pipeline with CustomLabelEncoder
+    pipeline = Pipeline([
+        ('preprocessing', ColumnTransformer([
+            ('label_encoding', CustomLabelEncoder(['Type', 'Delivery Status', 'Order Status', 'Shipping Mode']), ['Type', 'Delivery Status', 'Order Status', 'Shipping Mode']),
+            ('target_encoding', ce.TargetEncoder(cols=['Category Name', 'Customer City', 'Customer State', 'Market', 'Order City', 'Order Country', 'Order Region', 'Order State', 'Product Name']), ['Category Name', 'Customer City', 'Customer State', 'Market', 'Order City', 'Order Country', 'Order Region', 'Order State', 'Product Name']),
+            ('scaling', MinMaxScaler(), ['Days for shipping (real)', 'Days for shipment (scheduled)'])
+        ])),
+        ('feature_selection', SelectKBest(score_func=chi2, k=15)),
+        ('classification', DecisionTreeClassifier(random_state=42))
+    ])
+
+    # Fit the pipeline
+    pipeline.fit(X_train, y_train)
+
+    # Save the pipeline
+    joblib.dump(pipeline, 'pipeline_1.pkl')
+
+    # Save unique values
+    unique_values = {}
+    categorical_columns = ['Type', 'Delivery Status', 'Category Name', 'Customer City', 'Customer State',
+                           'Market', 'Order City', 'Order Country', 'Order Region', 'Order State',
+                           'Order Status', 'Product Name', 'Shipping Mode']
+    for col in categorical_columns:
+        unique_values[col] = X_train[col].unique().tolist()
+    with open('unique_values.pkl', 'wb') as file:
+        pickle.dump(unique_values, file)
+
+if __name__ == '__main__':
+    main()
